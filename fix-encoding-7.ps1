7a5acda5b0b9a51d1a920021ab17b748090c20ef
Cecile Royer <cilroy96@live.fr>
2026-02-21 23:43:00 +0100

    a

--- fix-encoding-7.ps1
+++
@@ -1,8 +0,0 @@
-﻿$files = Get-ChildItem -Path "C:\Users\cilro\Documents\UI designer\Portfolio\*.html"
-foreach ($file in $files) {
-  $text = Get-Content -Raw -Encoding UTF8 $file.FullName
-  $text = $text.Replace('>?</a>', '>↑</a>')
-  $text = $text.Replace('<span class="project-nav__chevron"><</span>', '<span class="project-nav__chevron">‹</span>')
-  $text = $text.Replace('<span class="project-nav__chevron">></span>', '<span class="project-nav__chevron">›</span>')
-  Set-Content -Path $file.FullName -Value $text -Encoding UTF8
-}
